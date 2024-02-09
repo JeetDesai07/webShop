f96d41df79d01d24d4230c46321d3359847fabab
JeetDesai07 <vampiredesai@gmail.com>
2024-02-09 12:59:50 +0530

    updated files

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+fetchProducts();
+
+// fetch Products-Data!!
+async function fetchProducts() {
+
+    const rawData = await fetch("https://fakestoreapi.com/products");
+
+    const data = await rawData.json();
+
+    // console.log(data);
+
+    // products !!
+    const productContainer = document.querySelector(".products");
+    productContainer.innerHTML = ``;
+
+    let productTemplate = ``;
+
+    // display products.
+    for (let i = 0; i < data.length; i++) {
+
+        productTemplate += `
+    
+            <div class="card product-card" style="width: 18rem;">
+                <img src="${data[i].image}" class="rounded mx-auto d-block" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${data[i].title}</h5>
+                    <p class="card-text">${data[i].description}</p>
+                    <p class="card-text">₹ ${Math.round(data[i].price)}</p>
+                    <p class="card-text">&bigstar; ${data[i].rating.rate} | ${data[i].rating.count}</p>
+
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                </div>
+            </div>
+    
+        `;
+
+    }
+
+
+    productContainer.innerHTML = productTemplate;
+
+
+}
+
+
